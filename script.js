5c3e92b535e959ec4573724de5569c0ff856941a
jonny0608 <527jlanier@frhsd.com>
2026-01-29 09:24:05 -0500

    Kilogore work day 1/29/2026

---
+++ script.js
@@ -0,0 +1,22 @@
+let quotesArray = ['NAPS is by far the most significant cultural force of the decade. — New York Times',
+'These days sleeping at your desk is not just acceptable, it’s encouraged. — The Atlantic',
+'NAPS has changed the way people think about sleep, on a grand scale. — Washington Post',
+'Happiness increased 200% since NAPS began promoting siestas. — National Science Review',
+'NAPS has ushered in a new era of sleep. — USA Today',
+'Not surprisingly, more siestas means more productivity. — Wall Street Journal',
+'NAPS is doing the hard work of helping people get the rest they need. — U.S. News'];
+
+function generateRandomNUmber () {
+let rndmnum = Math.floor(Math.random()*7)
+return rndmnum
+}
+function generateQuote(Num) {
+document.getElementById('press-quote').textContent = quotesArray[Num]
+}
+
+function buildPage(){
+    
+    generateQuote(generateRandomNUmber())
+}
+
+buildPage()
